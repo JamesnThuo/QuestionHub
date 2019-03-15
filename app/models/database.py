4fcc792ff5afedfebd30b2ec6a8b9e2b6c2f9f98
James <jamesnthuo@gmail.com>
2019-03-15 21:17:40 +0300

    [ft

---
+++ app/models/database.py
@@ -0,0 +1,46 @@
+import psycopg2
+from .migration import set_up_tables, drop_table_if_exists
+
+class DatabaseConnection:
+    """Handles the main connection to the database of the app setting"""
+    def __init__(self,db_url):
+        """"Initialize the class intance to take a database url as a parameter"""
+        try:
+            global conn,cur
+
+            #connection
+            conn=psycopg2.connect(db_url)
+            cur=conn.cursor()
+        except Exception as error:
+            print(error)
+    
+    def create_tables(self,query):
+        """creating the tables in migration"""
+        tables_to_create=set_up_tables()
+        for query in tables_to_create:
+            cur.execute(query)
+            conn.commit()
+    
+    def drop_tables(self,query):
+        """Drops tables in the database"""
+        tables_to_drop=drop_table_if_exists()
+        for query in tables_to_drop:
+            cur.execute(query)
+            conn.commit()
+    
+    def fetch_single_row(self,query):
+        """Fetches a single row in a table"""
+        cur.execute(query)
+        fetchedRow=cur.fetchone()
+        return fetchedRow
+    
+    def saving_or_editing(self,query):
+        """Saves or edits data in the database"""
+        cur.execute(query)
+        conn.commit()
+    
+    def fetch_all_rows(self,query):
+        """Fetches all rows in a table"""
+        cur.execute(query)
+        all_rows=cur.fetchall()
+        return all_rows
---
+++ app/models/migration.py
@@ -0,0 +1,48 @@
+import os
+import datetime
+
+def drop_table_if_exists():
+    """ Deletes all tables"""
+    drop_users = """ DROP TABLE IF EXISTS users """
+    drop_questions = """ DROP TABLE IF EXISTS questions """
+    drop_answers= """ DROP TABLE IF EXISTS answers """
+    drop_upvotes= """ DROP TABLE IF EXISTS upvote """
+    return [drop_questions, drop_users, drop_answers, drop_upvotes]
+
+def set_up_tables():
+    create_users_table="""
+        CREATE TABLE IF NOT EXISTS users (
+            id serial PRIMARY KEY,
+            firstname VARCHAR(50) NOT NULL,
+            lastname VARCHAR(50) NOT NULL,
+            email VARCHAR(50) NOT NULL,
+            password VARCHAR(50) NOT NULL,
+            registerdate TIMESTAMP
+        );
+    """
+    create_questions_table="""
+        CREATE TABLE IF NOT EXISTS questions (
+            id serial PRIMARY KEY,
+            description VARCHAR(50),
+            postedOn TIMESTAMP,
+            user_id INT
+        );"""
+    create_answer_table="""
+        CREATE TABLE IF NOT EXISTS answers (
+            id serial PRIMARY KEY,
+            description VARCHAR(50),
+            user_id INT,
+            question_id INT,
+            postedOn TIMESTAMP,
+            isCorrect BOOLEAN DEFAULT FALSE
+        );"""
+    create_upvote_table="""
+        CREATE TABLE IF NOT EXISTS upvote (
+            answer_id INT,
+            user_id INT
+        );"""
+    
+    return [create_users_table,create_questions_table,create_answer_table,create_upvote_table]
+
+
+
