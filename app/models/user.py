25f9ead80255b2696137d7adbce5d6c1f6981d6e
James <jamesnthuo@gmail.com>
2019-03-15 23:21:16 +0300

    [finishes #16467403] Created user model

---
+++ app/models/user.py
@@ -0,0 +1,24 @@
+"""User model"""
+import datetime
+import psycopg2
+from .database import DatabaseConnection as db_conn
+
+time_now=datetime.datetime.utcnow()
+
+class User(db_conn):
+    def __init__(self, userdata):
+        self.firstname=userdata[0]
+        self.secondname=userdata[1]
+        self.email=userdata[2]
+        self.password=userdata[3]
+        self.registerdate=time_now
+
+    """Registering a new user"""
+    def create_new_user(self,userdata):
+        query=""" INSERT INTO users (firstname, secondname, email, password, registerdate) VALUES ('{}','{}','{}','{}',{}')
+        """ .format(self.firstname,self.secondname,self.email,self.password,self.registerdate)
+        
+        """passing to database function to save user"""
+        self.saving_or_editing(query)
+        
+
