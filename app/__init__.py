d1203bd4b791f2fa5c1a4adc6ee8c57b58d1c9a0
James <jamesnthuo@gmail.com>
2019-03-15 10:34:50 +0300

    [chore #164646901] Finalizing on application structure

--- app/__init__.py
+++ app/__init__.py
@@ -6,8 +6,9 @@ from flask import Flask, Blueprint, jsonify
 def create_app(name_conf):
     # We will be using the config variable to determine the database
     app = Flask(__name__, instance_relative_config=True)
+    
     app.config.from_object(app_config[name_conf])
     app.config.from_pyfile('config.py')
     # db_url = app_config[name_conf].Database_Url
-
+    
     return app
