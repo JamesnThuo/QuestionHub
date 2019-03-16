443691af033e92f8ce32d801fb4d566a0e36d983
James <jamesnthuo@gmail.com>
2019-03-16 08:37:26 +0300

    [Start #164677455] Initial commit to start the story

--- app/models/migration.py
+++ app/models/migration.py
@@ -10,25 +10,21 @@ def drop_table_if_exists():
     return [drop_questions, drop_users, drop_answers, drop_upvotes]
 
 def set_up_tables():
-    create_users_table="""
-        CREATE TABLE IF NOT EXISTS users (
+    create_users_table=""" CREATE TABLE IF NOT EXISTS users (
             id serial PRIMARY KEY,
             firstname VARCHAR(50) NOT NULL,
             lastname VARCHAR(50) NOT NULL,
             email VARCHAR(50) NOT NULL,
             password VARCHAR(50) NOT NULL,
             registerdate TIMESTAMP
-        );
-    """
-    create_questions_table="""
-        CREATE TABLE IF NOT EXISTS questions (
+        ); """
+    create_questions_table="""CREATE TABLE IF NOT EXISTS questions (
             id serial PRIMARY KEY,
             description VARCHAR(50),
             postedOn TIMESTAMP,
             user_id INT
         );"""
-    create_answer_table="""
-        CREATE TABLE IF NOT EXISTS answers (
+    create_answer_table="""CREATE TABLE IF NOT EXISTS answers (
             id serial PRIMARY KEY,
             description VARCHAR(50),
             user_id INT,
@@ -36,8 +32,7 @@ def set_up_tables():
             postedOn TIMESTAMP,
             isCorrect BOOLEAN DEFAULT FALSE
         );"""
-    create_upvote_table="""
-        CREATE TABLE IF NOT EXISTS upvote (
+    create_upvote_table="""CREATE TABLE IF NOT EXISTS upvote (
             answer_id INT,
             user_id INT
         );"""
